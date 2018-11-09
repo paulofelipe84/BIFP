bae000fa87c6fa599dd4ef3e898657fed82c73b1
Paulo Felipe Dias Barbosa <paulofelipe@gmail.com>
2018-11-09 23:20:47 +1100

    Update BiffyPlatinum.sol

--- BiffyPlatinum.sol
+++ BiffyPlatinum.sol
@@ -6,14 +6,14 @@ pragma solidity ^0.4.25;
 contract BiffyPlatinum {
     address public owner;
 
-    string public name = 'Biffy Platinum'; // Change it to your Token Name.
-    string public symbol = 'BIFP'; // Change it to your Token Symbol. Max 4 letters!
+    string public name = 'Biffy Platinum';
+    string public symbol = 'BIFP';
   
-    string public standard = 'Token 0.1'; // Do not change this one.
+    string public standard = 'Token 0.1';
 
-    uint8 public decimals = 8; // It's recommended to set decimals to 8.
+    uint8 public decimals = 8;
 
-    uint256 public totalSupply = 10000000000; // Change it to the Total Supply of your Token.
+    uint256 public totalSupply = 10000000000;
     
     uint256 public threshold = 100; // Maximum number of tokens that can be played for the BIFP-only prize.
 
@@ -30,7 +30,7 @@ contract BiffyPlatinum {
     mapping (string => uint256) internal prizesBalances;
     mapping (address => saleAttributes) public tokensForSale;
     
-    address public _player;
+    address public player;
 
     // This generates a public event on the blockchain that will notify clients
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
@@ -121,51 +121,86 @@ contract BiffyPlatinum {
     /***************************************************************
                         BIFP-Specific Functions
     ***************************************************************/
-    function upForGrabs(uint256 amount) public
-        returns (bool win, uint256 rewardAmount) {
+    
+    //SafeMath
+    function safeAdd(uint256 x, uint256 y) pure internal returns(uint256) {
+      uint256 z = x + y;
+      assert((z >= x) && (z >= y));
+      return z;
+    }
 
-            _player = msg.sender;
+    function safeSubtract(uint256 x, uint256 y) pure internal returns(uint256) {
+      assert(x >= y);
+      uint256 z = x - y;
+      return z;
+    }
 
-            require(balanceOf[_player] >= amount);
-            require(amount > 0);
+    function safeMult(uint256 x, uint256 y) pure internal returns(uint256) {
+      uint256 z = x * y;
+      assert((x == 0)||(z/x == y));
+      return z;
+    }
+    
+    modifier onlyOwner()
+    {
+        require(msg.sender == owner, "Sender not authorized.");
+        _;
+    }
+    
+    function upForGrabs(uint amount) public
+        returns (bool win, uint rewardAmount) {
+
+            player = msg.sender;
+
+            require(amount > 0, "Amount needs to be higher than zero.");
+            require(balanceOf[player] >= amount, "Not enough balance.");
 
             if (prizesBalances["upForGrabs"] > 0){
-                _transfer(owner, _player, prizesBalances["upForGrabs"]);
+                _transfer(owner, player, prizesBalances["upForGrabs"]);
+                prizesBalances["upForGrabs"] = 0;
 
                 win = true;
                 rewardAmount = prizesBalances["upForGrabs"];
-
-                prizesBalances["upForGrabs"] = 0;
+                
             } else {
-                _transfer(_player, owner, amount);
-
-                win = false;
-                rewardAmount = 0;
+                _transfer(player, owner, amount);
 
                 prizesBalances["upForGrabs"] = amount;
+                
+                win = false;
+                rewardAmount = 0;
+                
             }
     }// upForGrabs(uint amount)
+    
+    function loadUpForGrabs(uint amount) onlyOwner public{
+        
+        require(amount > 0, "Amount needs to be higher than zero.");
+        require(balanceOf[owner] >= amount, "Not enough balance.");
+        
+        safeAdd(prizesBalances["upForGrabs"], amount);
+        
+    }
 
-    function setThreshold(uint256 value) public {
-        // Will value come in with the 8 additional decimal places or does it need to be converted here?
-        threshold = value;
+    function setThreshold(uint value) onlyOwner public {
+        threshold = value * 10 ** uint256(decimals);
     }// end setThreshold
 
     function addToHtmlPrize() public payable {
     // Anyone can manually add to the prize if they're feeling generous.
-
-        require(msg.value > 0);
-        prizesBalances["htmlcoinLotteryPrize"] += msg.value;
+    
+        require(msg.value > 0, "Value needs to be higher than zero.");
+        safeAdd(prizesBalances["htmlcoinLotteryPrize"], msg.value);
     }// end addToHtmlPrize
 
-    function addToTokenPrize(uint256 value) public {
+    function addToTokenPrize(uint value) public {
     // Anyone can manually add to the prize if they're feeling generous.
 
         require(value > 0);
-        prizesBalances["tokenLotteryPrize"] += value;
+        safeAdd(prizesBalances["tokenLotteryPrize"], value);
     }// end addToTokenPrize
 
-    function setSell(uint256 quantity, uint256 htmlPrice) public {
+    function setSell(uint quantity, uint htmlPrice) public {
     // Users, including the owner, can sell their own BIFP for whatever price they want.
 
         require(quantity <= balanceOf[msg.sender] && quantity > 0);
@@ -175,7 +210,7 @@ contract BiffyPlatinum {
         tokensForSale[msg.sender].pricePerToken = htmlPrice;
     }// end setSell 
 
-    function buyTokens(address _seller, uint256 qtyToBuy) payable public {
+    function buyTokens(address _seller, uint qtyToBuy) payable public {
         // Can't buy from yourself.
         require(msg.sender != _seller);
 
@@ -189,8 +224,8 @@ contract BiffyPlatinum {
 
     }// end buyTokens
 
-    function playLottery(uint256 value) public
-        returns (bool win, uint256 rewardAmount) {
+    function playLottery(uint value) public
+        returns (bool win, uint rewardAmount) {
         // This function delegates which lottery process should run.  Might also handle the payouts.
 
             // You have to play more than 0 to win.  :P
@@ -207,8 +242,8 @@ contract BiffyPlatinum {
             win = (rewardAmount > 0);
     }// end playLottery
 
-    function playTokenLottery(uint256 value) public
-        returns (uint256 rewardAmount) {
+    function playTokenLottery(uint value) public
+        returns (uint rewardAmount) {
              uint randomNumber;
              
             // uint randomNumber = SOME PROCESS to do random number betwen 0 and tokenLotteryChances, with some value being the winning number.
@@ -219,8 +254,8 @@ contract BiffyPlatinum {
             }
     }// end playTokenLottery
 
-    function playHtmlcoinLottery(uint256 value) public
-        returns (uint256 rewardAmount) {
+    function playHtmlcoinLottery(uint value) public
+        returns (uint rewardAmount) {
             uint randomNumber;
             
             // uint randomNumber = SOME PROCESS to do random number betwen 0 and htmlcoinLotteryChances, with some value being the winning number.
